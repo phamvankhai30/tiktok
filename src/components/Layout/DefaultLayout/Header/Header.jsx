93e628bd875355ef01d80510ec4caaca89f89a93
unknown <phamvankhai306@gmail.com>
2022-05-25 02:31:11 +0700

    header

--- src/components/Layout/DefaultLayout/Header/Header.jsx
+++
@@ -1,5 +0,0 @@
-function Header() {
-  return <h2>Giao diện Header</h2>;
-}
-
-export default Header;
--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,13 +1,16 @@
-import Header from './Header/Header';
-import Sidebar from './Sidebar/Sidebar';
+import Header from '../components/Header/Header';
+import Sidebar from '../components/Sidebar/Sidebar';
+import classNames from 'classnames/bind';
+import styles from './DefaultLayout.module.scss';
 
+const cx = classNames.bind(styles);
 export default function DefaultLayout({ children }) {
   return (
-    <div>
+    <div className={cx('wrapper')}>
       <Header />
-      <div>
+      <div className={cx('container')}>
         <Sidebar />
-        <div>{children}</div>
+        <div className={cx('contents')}>{children}</div>
       </div>
     </div>
   );
